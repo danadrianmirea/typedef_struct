46287601d68ce3d130adddf3c5e2edf0e602a9e4
Dan Adrian Mirea <damirea84@gmail.com>
2024-09-20 19:06:41 +0300

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# typedef_struct" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/typedef_struct.git
-git push -u origin main
